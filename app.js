ac37167b52d9f53a14c8e8e6a4977e067e065f02
pixprocoder <makkobir79@gmail.com>
2025-02-08 17:07:32 +0200

    server setup done

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+import express from "express";
+const app = express();
+
+app.use("/", (req, res) => {
+  res.send({ say: "hello" });
+});
+
+const port = 6001;
+
+app.listen(port, () => console.log("listening on " + port));
+
+export default app;
